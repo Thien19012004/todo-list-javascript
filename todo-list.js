e0dbb25e8586cda2c931b860457d8ec029c135f0
DoanMinhThuan <thuandoan090104@gmail.com>
2024-12-02 20:30:46 +0700

    Add todo-list project

---
+++ todo-list.js
@@ -0,0 +1,30 @@
+const todoList =[];
+
+function add(){
+  const inputName = document.querySelector('.js-todo-box');
+  const name = inputName.value;
+  const inputDate = document.querySelector('.js-date-box');
+  const dueDate = inputDate.value;
+  todoList.push({name, dueDate});
+  inputName.value = '';
+  inputDate.value = '';
+  renderTodoList();
+}
+function renderTodoList(){
+  let todoListHTML = '';
+  for (let i= 0; i< todoList.length; i++){
+    const todoObject = todoList[i];
+    const name = todoObject.name;
+    const dueDate = todoObject.dueDate;
+    const html = `
+    <div class="name">${name}</div> 
+    <div class="dueDate">${dueDate}</div>
+    <button class="delete" onclick="
+    todoList.splice(${i}, 1);
+    renderTodoList();
+    ">Delete</button>
+    `;
+    todoListHTML += html;
+  }
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
